5a6a37794b479c3f27cef8c75de1cbaa6be30322
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-10 21:20:41 +0700

    Update index.ts

--- types/index.ts
+++ types/index.ts
@@ -9,3 +9,28 @@ export type ProductEditModalProps = {
   suplier: suplier;
   foto: string;
 };
+
+export type ProductWithSupplier = {
+  id: number;
+  nama: string;
+  deskripsi: string;
+  harga: number;
+  stok: number;
+  foto: string;
+  suplier_id: number;
+  suplier: suplier;
+};
+
+export type SupplierCardProps = {
+  id_suplier: number;
+  nama_suplier: string;
+  alamat: string;
+  email: string;
+};
+
+export type SupplierEditProps = {
+  id_suplier: number;
+  nama_suplier: string;
+  alamat: string;
+  email: string;
+};
