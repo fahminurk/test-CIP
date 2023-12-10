fc3bf37c856091f2e13d52a069d0d3624eb5f8de
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-10 21:18:56 +0700

    update: moving schema

--- helper/schema.ts
+++ helper/schema.ts
@@ -7,3 +7,23 @@ export const editProductSchema = z.object({
   stok: z.string().min(1, { message: "required" }),
   suplier: z.string().min(1, { message: "required" }),
 });
+
+export const addProductSchema = z.object({
+  nama: z.string().min(2, { message: "reqired" }).max(50),
+  deskripsi: z.string().min(2, { message: "required" }).max(50),
+  harga: z.string().min(1, { message: "required" }).max(1000000),
+  stok: z.string().min(1, { message: "required" }).max(100),
+  suplier: z.string().min(1, { message: "required" }),
+});
+
+export const editSupplierSchema = z.object({
+  nama_suplier: z.string().min(2).max(50),
+  alamat: z.string().min(2).max(50),
+  email: z.string().email(),
+});
+
+export const addSupplierSchema = z.object({
+  nama_suplier: z.string().min(2).max(50),
+  alamat: z.string().min(2).max(50),
+  email: z.string().email(),
+});
